949fc6af6c8656009d23d53913fe438a6a99a7f7
Pijar Karisma <pijarkarisma@gmail.com>
2023-06-09 12:34:38 +0700

    fetch data from db, ui revision

--- src/screens/Home/DetailNewsScreen.js
+++ src/screens/Home/DetailNewsScreen.js
@@ -3,23 +3,21 @@ import React from 'react'
 
 const DetailNewsScreen = ({ route }) => {
     const data = route.params.param
-    
+
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: "#fff", position: 'relative' }}>
             <ScrollView style={{ paddingHorizontal: 20, paddingTop: 10 }}>
 
                 <Image
-                    source={{
-                        uri: data.image
-                    }}
+                    source={{ uri: `data:image/jpeg;base64,${data?.image}` }}
                     style={{ width: '100%', height: 290 }}
                 />
                 <Text style={{ textAlign: 'center', fontWeight: 'bold', marginVertical: 10 }}>
                     {data?.title}
                 </Text>
-                
-                <Text style={{ textAlign: 'justify', marginTop:10  }}>
-                    {data?.description}
+
+                <Text style={{ textAlign: 'justify', marginTop: 10 }}>
+                    {data?.desc}
                 </Text>
             </ScrollView>
         </SafeAreaView>
--- src/screens/Home/NewsView.js
+++ src/screens/Home/NewsView.js
@@ -3,24 +3,52 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import ItemCardNews from '../../components/HomeScreen/ItemCardNews'
 import { useNavigation } from '@react-navigation/native'
-
-
-
+import { LOCAL_DEVICE_IP } from "@env"
+import { Buffer } from 'buffer'
+import moment from 'moment'
 
 const NewsView = () => {
+  const ip = LOCAL_DEVICE_IP
   const [newsData, setNewsData] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const navigation = useNavigation()
 
+  const calculateReadTime = (text) => {
+    const wpm = 225
+    const words = text.trim().split(/\s+/).length
+    const time = Math.ceil(words / wpm)
+    return time
+  }
+
+  const getAllNews = async () => {
+    setIsLoading(true)
+    axios.get(`${ip}/news`)
+      .then(function (res) {
+        const resData = res.data.news
+        const cleanedNewsData = []
+        resData.map(data => {
+          const base64Image = Buffer.from(data.image).toString('base64')
+          const readTime = calculateReadTime(data.desc)
+          cleanedNewsData.push({ ...data, image: base64Image, readTime: readTime })
+        })
+        setNewsData(cleanedNewsData)
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
+      .finally(function () {
+        setIsLoading(false)
+      })
+  }
 
   useEffect(() => {
-    setIsLoading(true)
-    axios.get('  https://fakestoreapi.com/products')
-      .then(response => response.data)
-      .then(res => setNewsData(res))
-      .catch(error => console.error(error))
-      .finally(() => setIsLoading(false))
-
+    // setIsLoading(true)
+    // axios.get('  https://fakestoreapi.com/products')
+    //   .then(response => response.data)
+    //   .then(res => setNewsData(res))
+    //   .catch(error => console.error(error))
+    //   .finally(() => setIsLoading(false))
+    getAllNews()
   }, [])
 
   const tambahData = () => {
@@ -46,28 +74,43 @@ const NewsView = () => {
           </View> :
           <FlatList
             data={newsData}
-            keyExtractor={item => item.id.toString()}
+            keyExtractor={item => item.id}
             onEndReached={() => tambahData}
             renderItem={({ item }) => (
               <TouchableOpacity
                 onPress={() => navigation.navigate('DetailNews', { param: item })}
-                style={{ flexDirection: 'row', marginBottom: 5, padding: 5, marginTop: 10 }}
+                style={{ flexDirection: 'row', marginBottom: 10, padding: 5, marginTop: 10 }}
               >
 
                 <Image
-                  source={{ uri: item.image }}
-                  style={{ width: 50, height: 50 }}
+                  source={{ uri: `data:image/jpeg;base64,${item.image}` }}
+                  style={{ width: 110, height: 110, borderRadius: 8 }}
                 />
-                <View style={{ marginLeft: 5 }}>
-                  <Text style={{ fontWeight: 'bold', fontSize: 16 }}>
-                    {item.title.length > 20 ? `${item.title.slice(0, 20)}..` : item.title}
+                <View style={{ marginLeft: 12, flexWrap: 'wrap', alignItems: 'flex-start', flex: 1, justifyContent: 'space-between' }}>
+                  <Text style={{ fontWeight: 'bold', fontSize: 16 }} numberOfLines={3}>
+                    {/* {testString.length > 150 ? `${item.title.slice(0, 150)}..` : testString} */}
+                    {item.title}
                   </Text>
-                  <Text style={{ fontWeight: '500', color: '#b1b1b1', fontSize: 10 }}>
-                    2023-10-19
-                  </Text>
-                  {/* <Text style={{ fontWeight: '500', color: '#b1b1b1', fontSize: 10 }}>
-                    {item.penulis} / {item.createdAt}
-                  </Text> */}
+                  <View>
+                    <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                      <Image
+                        source={require('../../../assets/clock.png')}
+                        style={{ width: 16, height: 16 }}
+                      />
+                      <Text style={{ color: 'black', fontSize: 12, marginLeft: 4 }}>
+                        {item.readTime} menit baca
+                      </Text>
+                    </View>
+                    <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginTop: 6 }}>
+                      <Image
+                        source={require('../../../assets/date.png')}
+                        style={{ width: 16, height: 16 }}
+                      />
+                      <Text style={{ color: 'black', fontSize: 12, marginLeft: 4 }}>
+                        {moment(item.modifiedAt).format('LLL')}
+                      </Text>
+                    </View>
+                  </View>
                 </View>
               </TouchableOpacity>
             )}
@@ -77,13 +120,12 @@ const NewsView = () => {
                   style={{
                     height: 1,
                     width: "100%",
-                    backgroundColor: "#000",
+                    backgroundColor: "#BFBFBF"
                   }}
                 />
               );
             }}
           />
-
       }
 
     </View>
